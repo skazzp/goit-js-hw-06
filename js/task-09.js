775839bfa3909c74e95e54a7b7b72205ea9e0703
skazzp <skazzp@gmail.com>
2022-08-08 19:20:36 +0300

    add task 9 + part 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorButton = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+colorButton.addEventListener('click', () => {
+  let bgColor = getRandomHexColor();
+  document.body.style.backgroundColor = `${bgColor}`;
+  span.textContent = `${bgColor}`;
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const number = document.querySelector('#controls > input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+console.dir(number);
+createBtn.addEventListener('click', () => {
+  const divCount = +number.value;
+  console.log(divCount);
+  number.value = '';
+  let size = 30;
+  for (let i = 0; i < divCount; i += 1) {
+    let color = getRandomHexColor();
+    const markup = `<div class="div">${color}</div>`;
+    boxesDiv.insertAdjacentHTML('beforeend', markup);
+  }
+});
